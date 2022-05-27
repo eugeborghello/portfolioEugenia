86d5779e9b874649e77b96ae9c9005b5c87671a1
Eugenia Borghello <eugenia.borghello@craneo.io>
2022-05-27 16:04:34 -0300

    crear portfolio con react

---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+
+const Layout = () => {
+    return (
+    <>Sidebar</>
+    )
+}
+
+export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+//import { Link } from 'react-router-dom';
+import './index.scss';
+
+const Sidebar = () => (
+<div classname='nav-bar'>
+    {/* <Link className='logo' to='/'>
+        <img  />
+    </Link> */}
+</div>)
+
+export default Sidebar;
